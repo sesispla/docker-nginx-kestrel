a7bcbe9eeba2969dae7b25258a55ba3d60e0296a
Sergio Sisternes <sesispla@outlook.com>
2016-06-11 11:30:39 +0200

    Missing using in Program.cs

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace WebApplicationBasic
 {
